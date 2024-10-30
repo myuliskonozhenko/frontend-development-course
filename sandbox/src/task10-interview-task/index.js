59570040efb5244b1710ce7fd94b90036f87772c
myuliskonozhenko <mary_yuryeva@icloud.com>
2024-10-31 01:25:35 +0300

    Update index.js

--- sandbox/src/task10-interview-task/index.js
+++ sandbox/src/task10-interview-task/index.js
@@ -6,5 +6,13 @@
  * @returns {Number}
  */
 export const solutionFn = (num) => {
-    // Ваш код здесь
+    while (n >= 10) {
+        let product = 1;
+        while (n > 0) {
+            product *= n % 10; 
+            n = Math.floor(n / 10); 
+        }
+        n = product; 
+    }
+    return n; 
 };
