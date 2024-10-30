b5b39e57417db8821fdf246451088913e74fd533
myuliskonozhenko <mary_yuryeva@icloud.com>
2024-10-31 00:36:17 +0300

    Update index.js

--- sandbox/src/task4-no-more-jokes/index.js
+++ sandbox/src/task4-no-more-jokes/index.js
@@ -6,5 +6,18 @@
  * @returns {Number} Сумма операндов
  */
 export const sum = (leftOperand, rightOperand) => {
-    // Ваш код здесь
+    const isLeftNumber = typeof left === 'number';
+    const isRightNumber = typeof right === 'number';
+
+    if (!isLeftNumber && !isRightNumber) {
+        throw new Error('Operands are not numbers');
+    }
+    if (!isLeftNumber) {
+        throw new Error('The left operand is not number');
+    }
+    if (!isRightNumber) {
+        throw new Error('The right operand is not number');
+    }
+
+    return left + right; // Возвращаем сумму, если оба операнда числа
 };
