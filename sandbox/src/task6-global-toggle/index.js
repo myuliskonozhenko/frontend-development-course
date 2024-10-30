6572654f642ea7ed7d1279aae15bc7a8519378e5
myuliskonozhenko <mary_yuryeva@icloud.com>
2024-10-31 01:09:52 +0300

    Update index.js

--- sandbox/src/task6-global-toggle/index.js
+++ sandbox/src/task6-global-toggle/index.js
@@ -5,5 +5,18 @@
  * @param {String} className Название класса
  */
 export const globalToggle = (className) => {
-    // Ваш код здесь
+    const elements = document.querySelectorAll(`.${className}`);
+    if (elements.length === 0) {
+        return;
+    }
+    const isActive = elements[0].classList.contains(`${className}_active`);
+    elements.forEach(element => {
+        if (isActive) {
+            element.classList.remove(`${className}_active`);
+            element.classList.add(className);
+        } else {
+            element.classList.remove(className);
+            element.classList.add(`${className}_active`);
+        }
+    });
 };
