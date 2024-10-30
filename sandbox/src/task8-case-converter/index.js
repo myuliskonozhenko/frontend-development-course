13058d92c2af6e6967ca3d0606ebe8ab2747cf3d
myuliskonozhenko <mary_yuryeva@icloud.com>
2024-10-31 01:21:28 +0300

    Update index.js

--- sandbox/src/task8-case-converter/index.js
+++ sandbox/src/task8-case-converter/index.js
@@ -6,5 +6,12 @@
  * @returns {String} Строка в camelCase
  */
 export const solutionFn = (snakeCaseString) => {
-    // Ваш код здесь
+    const words = snakeCaseString.split('_');
+    if (words.length === 1) {
+        return words[0];
+    }
+    for (let i = 1; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join('');
 };
